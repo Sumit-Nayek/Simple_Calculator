589dfe17bf1b3ff7ce953c9d7a679293f550d5d2
Sumit Nayek <63765654+Sumit-Nayek@users.noreply.github.com>
2025-11-28 01:49:16 +0000

    Second commit

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ import streamlit as st
 st.write('# Streamlit calculator')
 number1= st.number_input('number 1')
 number2 = st.number_input('number 2')
-num3 = number1+number2
+number3 = st.number_input('number 3')
+num3 = number1+number2+number3
 st.write('# Answer is ',num3)
